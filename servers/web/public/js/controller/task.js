1201fd891918a2ee6e686650b60981647047c968
Matheus Medeiros <matheus.m.sarmento@gmail.com>
2018-04-10 00:10:06 -0300

    Reforms add screen
    New components are separated in files

--- servers/web/public/js/controller/task.js
+++ servers/web/public/js/controller/task.js
@@ -91,6 +91,10 @@ app.controller('addCtrl', function ($scope, $rootScope, $compile) {
     }
   }
 
+  $scope.addTaskForm = {
+    commandLine: ''
+  }
+
   $scope.parse = function (commandLine) {
     $scope.clear()
 
@@ -98,28 +102,99 @@ app.controller('addCtrl', function ($scope, $rootScope, $compile) {
       commandLine = ''
     }
 
-    const matches = commandLine.match(/(%tf|%ef|%n)/g)
+    const matches = commandLine.match(/(%tf|%ef|%n|%s)/g)
 
     if (!matches) {
       return
     }
 
+    const table = document.createElement("table");
+    table.className = "table table-sm"
+
+    {
+      let tableRow = document.createElement("tr");
+
+      {
+        let tableHeader = document.createElement("th");
+        tableHeader.innerHTML = "Precedence";
+        tableHeader.width = "10%"
+        tableRow.appendChild(tableHeader);
+      }
+
+      {
+        let tableHeader = document.createElement("th");
+        tableHeader.innerHTML = "Value";
+        tableRow.appendChild(tableHeader);
+      }
+
+      table.appendChild(tableRow)
+    }
+
     for (var match in matches) {
+      let tableRow = document.createElement("tr");
+
+      // Precedence
+      {
+        let tableCell = document.createElement("td");
+        let precedence = document.createElement("input");
+
+        precedence.className = "form-control form-control-success"
+        precedence.type = "number";
+        precedence.min = 1;
+        precedence.value = 1;
+
+        tableCell.appendChild(precedence)
+        tableRow.appendChild(tableCell);
+      }
+
+
       if (matches[match] === '%tf') {
-        const input = $compile('<textfile id="' + match + '"></textfile>')($scope)
+        const input = $compile('<textfile id="' + match + '" ng-required="true"></textfile>')($scope)
         angular.element(document.getElementById('inputContainer')).append(input)
       }
 
       if (matches[match] === '%ef') {
-        const input = $compile('<executablefile id="' + match + '"></executablefile>')($scope)
-        angular.element(document.getElementById('inputContainer')).append(input)
+        let tableCell = document.createElement("td");
+
+        const input = $compile('<executablefile id="' + match + '" ng-required="true"></executablefile>')($scope)
+        angular.element(tableCell).append(input)
+
+        tableRow.appendChild(tableCell);
       }
 
       if (matches[match] === '%n') {
+        let tableCell = document.createElement("td");
+
         const input = $compile('<numberform id="' + match + '"></numberform>')($scope)
+        angular.element(tableCell).append(input)
+
+        tableRow.appendChild(tableCell);
+      }
+
+      if (matches[match] === '%s') {
+        const input = $compile('<stringform id="' + match + '" ng-required="true"></stringform>')($scope)
         angular.element(document.getElementById('inputContainer')).append(input)
       }
+
+      table.appendChild(tableRow);
     }
+
+    document.getElementById('inputContainer').append(table);
+
+    const submit = $compile('<submit></submit>')($scope)
+    angular.element(document.getElementById('inputContainer')).append(submit)
+  }
+
+  $scope.addDirective = function (directive) {
+    $scope.clear()
+
+    if (!$scope.addTaskForm.commandLine) {
+      $scope.addTaskForm.commandLine = ''
+    }
+
+    const position = commandLine.selectionStart
+
+    $scope.addTaskForm.commandLine = [$scope.addTaskForm.commandLine.slice(0, position), directive, $scope.addTaskForm.commandLine.slice(position)].join('')
   }
 
 })
@@ -142,5 +217,12 @@ app.controller('addCtrl', function ($scope, $rootScope, $compile) {
       restrict: 'E',
       scope: { 'id': '@' },
       templateUrl: 'views/dashboard/forms/text_file_form.html'
+    }
+  })
+  .directive('submit', function () {
+    return {
+      restrict: 'E',
+      scope: { 'id': '@' },
+      templateUrl: 'views/dashboard/forms/submit.html'
     }
   })
