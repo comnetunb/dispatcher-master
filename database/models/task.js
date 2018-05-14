5b9f25666e377d303a0d173901da8e0218a48dd8
Mikael Mello <mikaelmmello@gmail.com>
2018-05-13 23:51:44 -0300

    Fix styling errors of app.js and database/ directory

--- database/models/task.js
+++ database/models/task.js
@@ -1,31 +1,31 @@
-﻿//!
-//! Version: MIT
-//!
-//! Portions created by Matheus Medeiros are Copyright (c) 2017-2018
-//! Matheus Medeiros. All Rights Reserved.
-//!
-//! Permission is hereby granted, free of charge, to any person obtaining a
-//! copy of this software and associated documentation files(the "Software"),
-//! to deal in the Software without restriction, including without limitation
-//! the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//! and / or sell copies of the Software, and to permit persons to whom the
-//! Software is furnished to do so, subject to the following conditions:
-//!
-//! The above copyright notice and this permission notice shall be included in
-//! all copies or substantial portions of the Software.
-//!
-//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//! DEALINGS IN THE SOFTWARE.
-//!
+﻿// !
+// ! Version: MIT
+// !
+// ! Portions created by Matheus Medeiros are Copyright (c) 2017-2018
+// ! Matheus Medeiros. All Rights Reserved.
+// !
+// ! Permission is hereby granted, free of charge, to any person obtaining a
+// ! copy of this software and associated documentation files(the "Software"),
+// ! to deal in the Software without restriction, including without limitation
+// ! the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// ! and / or sell copies of the Software, and to permit persons to whom the
+// ! Software is furnished to do so, subject to the following conditions:
+// !
+// ! The above copyright notice and this permission notice shall be included in
+// ! all copies or substantial portions of the Software.
+// !
+// ! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// ! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// ! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// ! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// ! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// ! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// ! DEALINGS IN THE SOFTWARE.
+// !
 
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const { Schema } = mongoose;
 
 const State = {
   PENDING: 0,
@@ -33,7 +33,7 @@ const State = {
   FINISHED: 2,
   CANCELED: 3,
   SENT: 4
-}
+};
 
 const taskSchema = Schema({
   _taskSet: {
@@ -73,55 +73,54 @@ const taskSchema = Schema({
   endTime: {
     type: Date
   }
-})
+});
 
-taskSchema.statics.State = State
+taskSchema.statics.State = State;
 
-taskSchema.statics.updateToDefaultState = function (taskId) {
-  const taskPopulate = { path: '_taskSet' }
+taskSchema.statics.updateToDefaultState = (taskId) => {
+  const taskPopulate = { path: '_taskSet' };
 
-  return model
+  return model // eslint-disable-line no-use-before-define
     .findById(taskId)
     .populate(taskPopulate)
-    .then(function (task) {
-      const taskSetState = task._taskSet.state
+    .then((task) => {
+      const taskSetState = task._taskSet.state;
 
-      task.worker = undefined
-      task.startTime = undefined
+      task.worker = undefined;
+      task.startTime = undefined;
 
-      const TaskSet = databaseRequire('models/task_set')
+      const TaskSet = databaseRequire('models/task_set'); // eslint-disable-line no-undef
 
       if (taskSetState === TaskSet.State.EXECUTING) {
-        task.state = State.PENDING
+        task.state = State.PENDING;
+      } else {
+        task.state = State.CANCELED;
       }
-      else {
-        task.state = State.CANCELED
-      }
 
-      return task.save()
-    })
-}
+      return task.save();
+    });
+};
 
-taskSchema.methods.isPending = function () {
-  return this.state === State.PENDING
-}
+taskSchema.methods.isPending = function () { // eslint-disable-line func-names
+  return this.state === State.PENDING;
+};
 
-taskSchema.methods.isSent = function () {
-  return this.state === State.SENT
-}
+taskSchema.methods.isSent = function () { // eslint-disable-line func-names
+  return this.state === State.SENT;
+};
 
-taskSchema.methods.isExecuting = function () {
-  return this.state === State.EXECUTING
-}
+taskSchema.methods.isExecuting = function () { // eslint-disable-line func-names
+  return this.state === State.EXECUTING;
+};
 
-taskSchema.methods.isFinished = function () {
-  return this.state === State.FINISHED
-}
+taskSchema.methods.isFinished = function () { // eslint-disable-line func-names
+  return this.state === State.FINISHED;
+};
 
-taskSchema.methods.isCanceled = function () {
-  return this.state === State.CANCELED
-}
+taskSchema.methods.isCanceled = function () { // eslint-disable-line func-names
+  return this.state === State.CANCELED;
+};
 
-const model = mongoose.model('Task', taskSchema)
+const model = mongoose.model('Task', taskSchema);
 
-module.exports = model
+module.exports = model;
--- database/models/task_set.js
+++ database/models/task_set.js
@@ -1,36 +1,37 @@
-﻿//!
-//! Version: MIT
-//!
-//! Portions created by Matheus Medeiros are Copyright (c) 2017-2018
-//! Matheus Medeiros. All Rights Reserved.
-//!
-//! Permission is hereby granted, free of charge, to any person obtaining a
-//! copy of this software and associated documentation files(the "Software"),
-//! to deal in the Software without restriction, including without limitation
-//! the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//! and / or sell copies of the Software, and to permit persons to whom the
-//! Software is furnished to do so, subject to the following conditions:
-//!
-//! The above copyright notice and this permission notice shall be included in
-//! all copies or substantial portions of the Software.
-//!
-//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//! DEALINGS IN THE SOFTWARE.
-//!
+﻿// !
+// ! Version: MIT
+// !
+// ! Portions created by Matheus Medeiros are Copyright (c) 2017-2018
+// ! Matheus Medeiros. All Rights Reserved.
+// !
+// ! Permission is hereby granted, free of charge, to any person obtaining a
+// ! copy of this software and associated documentation files(the "Software"),
+// ! to deal in the Software without restriction, including without limitation
+// ! the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// ! and / or sell copies of the Software, and to permit persons to whom the
+// ! Software is furnished to do so, subject to the following conditions:
+// !
+// ! The above copyright notice and this permission notice shall be included in
+// ! all copies or substantial portions of the Software.
+// !
+// ! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// ! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// ! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// ! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// ! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// ! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// ! DEALINGS IN THE SOFTWARE.
+// !
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 const State = {
   EXECUTING: 0,
   FINISHED: 1,
   CANCELED: 2
-}
+};
 
 const Priority = {
   MINIMUM: 0,
@@ -38,7 +39,7 @@ const Priority = {
   NORMAL: 2,
   HIGH: 3,
   URGENT: 4
-}
+};
 
 const taskSetSchema = Schema({
   _user: {
@@ -83,13 +84,13 @@ const taskSetSchema = Schema({
   remainingTasksCount: {
     type: Number
   }
-})
+});
 
-taskSetSchema.statics.State = State
-taskSetSchema.statics.Priority = Priority
+taskSetSchema.statics.State = State;
+taskSetSchema.statics.Priority = Priority;
 
 taskSetSchema.statics.UpdateRemainingTasksCount = (id) => {
-  const Task = databaseRequire('models/task')
+  const Task = databaseRequire('models/task'); // eslint-disable-line no-undef
 
   const taskFilter = {
     _taskSet: id,
@@ -98,18 +99,18 @@ taskSetSchema.statics.UpdateRemainingTasksCount = (id) => {
       { state: Task.State.SENT },
       { state: Task.State.EXECUTING }
     ]
-  }
+  };
 
   return Task
     .count(taskFilter)
-    .then(count => {
-      const taskSetFilter = { _id: id }
-      const taskSetUpdate = { remainingTasksCount: count }
+    .then((count) => {
+      const taskSetFilter = { _id: id };
+      const taskSetUpdate = { remainingTasksCount: count };
 
-      return model.update(taskSetFilter, taskSetUpdate)
-    })
-}
+      return model.update(taskSetFilter, taskSetUpdate); // eslint-disable-line
+    });
+};
 
-const model = mongoose.model('TaskSet', taskSetSchema)
+const model = mongoose.model('TaskSet', taskSetSchema);
 
-module.exports = model
+module.exports = model;
