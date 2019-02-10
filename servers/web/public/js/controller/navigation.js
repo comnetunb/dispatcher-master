8c6f6ae3b5547adba626eab44d9ca990fc21e905
Mikael Mello <mikaelmmello@gmail.com>
2019-02-09 23:00:22 -0200

    web: initial integration of front-end with backend notifications

--- servers/web/public/js/controller/navigation.js
+++ servers/web/public/js/controller/navigation.js
@@ -1,3 +1,11 @@
+const getNotifications = function ($scope, $http) {
+  $http
+    .get('/api/notifications')
+    .then(function (response) {
+      $scope.notifications = response.data;
+    });
+};
+
 app.controller('navigationCtrl', function ($scope, $rootScope, $http, $location) {
   $scope.signOut = function () {
     $http
@@ -7,4 +15,21 @@ app.controller('navigationCtrl', function ($scope, $rootScope, $http, $location)
         $location.path('/');
       });
   };
+
+  $scope.getResultClass = (result) => {
+    if (result === 'success') {
+      return 'text-success';
+    } else if (result === 'neutral') {
+      return 'text-info';
+    } else if (result === 'warning') {
+      return 'text-warning';
+    } else if (result === 'error') {
+      return 'text-danger';
+    }
+
+    return '';
+  };
+
+  $scope.notifications = [];
+  getNotifications($scope, $http);
 });
