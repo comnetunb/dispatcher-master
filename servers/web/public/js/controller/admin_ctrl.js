e93a5170b1d1afd7cc8fbbb33feb106946c98718
Mikael Mello <git@mikaelmello.com>
2019-06-15 13:22:39 -0300

    Add confirmation modal when approving an user

--- servers/web/public/js/controller/admin_ctrl.js
+++ servers/web/public/js/controller/admin_ctrl.js
@@ -8,7 +8,21 @@ const allowUser = function (userId, $scope, $http) {
     .post(`/api/user/allow/${userId}`);
 };
 
-app.controller('adminCtrl', function ($scope, $rootScope, $http, $interval, $routeParams) {
+app.controller('approveUserModalCtrl', function ($uibModalInstance, $scope, user) {
+  const $ctrl = this;
+
+  $scope.user = user;
+
+  $ctrl.ok = function () {
+    $uibModalInstance.dismiss('ok');
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
+
+app.controller('adminCtrl', function ($scope, $rootScope, $http, $uibModal) {
   $rootScope.sidebar = true;
   $scope.users = [];
 
@@ -19,19 +33,29 @@ app.controller('adminCtrl', function ($scope, $rootScope, $http, $interval, $rou
       });
   };
 
-  $scope.allow = function (userId) {
-    allowUser(userId, $scope, $http)
-      .then(() => {
-        let user = -1;
-        for (let i = 0; i < $scope.users.length; i += 1) {
-          if ($scope.users[i]._id == userId) {
-            user = i;
-          }
-        }
-        if (user != -1) {
-          $scope.users.splice(user, 1);
-        }
-      });
+  $scope.allow = function (user) {
+    openConfirmation(
+      $uibModal,
+      {
+        templateUrl: 'views/dashboard/modals/user_approval_confirm.html',
+        controller: 'approveUserModalCtrl',
+        resolve: { user },
+        callbackOk: () => {
+          allowUser(user._id, $scope, $http)
+            .then(() => {
+              let userIdx = -1;
+              for (let i = 0; i < $scope.users.length; i += 1) {
+                if ($scope.users[i]._id == user._id) {
+                  userIdx = i;
+                }
+              }
+              if (userIdx != -1) {
+                $scope.users.splice(userIdx, 1);
+              }
+            });
+        },
+      },
+    );
   };
 
   $scope.stop = function () {
