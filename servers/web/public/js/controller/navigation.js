1302b6f4bf6d60087db933dfaedb6a297f025b4c
Mikael Mello <git@mikaelmello.com>
2019-06-16 17:30:34 -0300

    Clicking on an unread notification will read it

--- servers/web/public/js/controller/navigation.js
+++ servers/web/public/js/controller/navigation.js
@@ -30,6 +30,20 @@ app.controller('navigationCtrl', function ($scope, $rootScope, $http, $location)
     return '';
   };
 
+  $scope.readNotification = function ($event, notification) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    $http
+      .post(`/api/notifications/${notification._id}`)
+      .then(function (response) {
+        const idx = $scope.notifications.indexOf(notification);
+
+        if (idx > -1) {
+          $scope.notifications.splice(idx, 1);
+        }
+      });
+  };
+
   $scope.notifications = [];
   getNotifications($scope, $http);
 });
