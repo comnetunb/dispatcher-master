a11919139ed3e971a8229052cc4d524ed5e84e45
Mikael Mello <git@mikaelmello.com>
2019-09-30 22:57:07 -0300

    client: Update auth service
    - Refactoring
    - Add register method

--- src/servers/web/client/src/app/services/auth.service.ts
+++ src/servers/web/client/src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from './user.service';
+import { RegisterUserRequest } from '../api/register-user-request';
 
 const apiRoute = '/api/users';
 
@@ -15,10 +17,12 @@ export class AuthService {
 
   private currentUserSubject: BehaviorSubject<IUser>;
   public currentUser: Observable<IUser>;
+  public redirectUrl: string;
 
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) {
     this.currentUserSubject = new BehaviorSubject<IUser>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
@@ -28,34 +32,46 @@ export class AuthService {
     return this.currentUserSubject.value;
   }
 
-  refresh() {
-    console.log('top');
-    console.log(`${apiRoute}/signed_in`);
+  private storeUser(user: IUser) {
+    if (user == null) return;
 
+    // store user details and jwt token in local storage to keep user logged in between page refreshes
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+
+  private eraseUser(): void {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  refresh() {
     const sub = this.http.get<IUser>(`${apiRoute}/signed_in`).subscribe((user) => {
-      if (user == null) return;
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      this.currentUserSubject.next(user);
+      this.storeUser(user);
       sub.unsubscribe();
     });
+  }
+
+  register(request: RegisterUserRequest) {
+    return this.userService.registerUser(request)
+      .pipe(map(user => {
+        this.storeUser(user);
+        this.router.navigate(['/dashboard']);
+      }));
   }
 
   login(username: string, password: string) {
     return this.http.post<IUser>(`${apiRoute}/sign_in`, { username, password })
       .pipe(map(user => {
-        // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-        this.router.navigate(['/']);
+        this.storeUser(user);
+        this.router.navigate(['/dashboard']);
       }));
   }
 
   logout() {
     this.http.post(`${apiRoute}/sign_out`, {}, { responseType: 'text' }).subscribe(() => {
-      // remove user from local storage and set current user to null
-      localStorage.removeItem('currentUser');
-      this.currentUserSubject.next(null);
+      this.eraseUser();
       this.router.navigate(['/']);
     });
   }
