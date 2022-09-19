ae57d3340a2ef8eee551939fb4d146de84a618ef
Mikael Mello <git@mikaelmello.com>
2022-09-18 18:56:17 -0700

    Fix docker publish script

--- docker-publish.sh
+++ docker-publish.sh
@@ -3,7 +3,7 @@ set -e -u -o pipefail
 
 CUR_VERSION=$(npm -s run env echo '$npm_package_version')
 
-docker build -t comnetunb/dispatcher-master:latest -t comnetunb/dispatcher-master:$CUR_VERSION .
+sudo docker build -t comnetunb/dispatcher-master:latest -t comnetunb/dispatcher-master:$CUR_VERSION .
 
-docker image push comnetunb/dispatcher-master:latest
-docker image push comnetunb/dispatcher-master:$CUR_VERSION
+sudo docker image push comnetunb/dispatcher-master:latest
+sudo docker image push comnetunb/dispatcher-master:$CUR_VERSION
