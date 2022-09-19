990e02e3bbdb758127d9a6dc3220d6fb70ff6754
Mikael Mello <git@mikaelmello.com>
2022-09-18 19:16:28 -0700

    Do not execute docker publish with npm one

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e -u -o pipefail
 
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  echo "Not running as root"
+  exit
+fi
+
 CUR_VERSION=$(npm -s run env echo '$npm_package_version')
 
-sudo docker build -t comnetunb/dispatcher-master:latest -t comnetunb/dispatcher-master:$CUR_VERSION .
+docker build -t comnetunb/dispatcher-master:latest -t comnetunb/dispatcher-master:$CUR_VERSION .
 
-sudo docker image push comnetunb/dispatcher-master:latest
-sudo docker image push comnetunb/dispatcher-master:$CUR_VERSION
+docker image push comnetunb/dispatcher-master:latest
+docker image push comnetunb/dispatcher-master:$CUR_VERSION
